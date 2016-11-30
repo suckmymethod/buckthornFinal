842b78f135cd4ef32344c8c93397598d70425949
jjarl12 <jonathan-jarl@bethel.edu>
2016-11-30 15:40:03 -0600

    Observation page added

---
+++ HTMLandCSS/New Observation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>New Observation</title>
+    <link rel="stylesheet" href="Stylesheet.css" type="text/css">
+</head>
+
+<body>
+
+
+</body>
+</html>
