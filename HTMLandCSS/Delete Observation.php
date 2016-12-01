c477040d86e183f380d42588cef6a95acc67a0fe
jjarl12 <jonathan-jarl@bethel.edu>
2016-12-01 12:12:40 -0600

    Added 2 more pages

---
+++ HTMLandCSS/Delete Observation.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Observation</title>
+    <link rel="stylesheet" href="Stylesheet.css" type="text/css">
+</head>
+<body>
+
+</body>
+</html>
