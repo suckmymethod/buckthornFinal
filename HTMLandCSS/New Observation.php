d362569053b177e54ff5156cbc7713d6772db773
k-larsen <CK@bethel-wifi.dhcp-138-219.vlan.net.its.bethel.edu>
2016-12-06 20:45:17 -0600

    New Observation Finished

--- HTMLandCSS/New Observation.php
+++ HTMLandCSS/New Observation.php
@@ -14,60 +14,60 @@
 
 <form class="table">
 
-        <p class="textbox-text">Date of Observation:</p>
-        <input type="text" name="ObservDate"/>
-        <i>(MM/DD/YYYY)</i>
+    <p class="textbox-text">Date of Observation:</p>
+    <input type="text" name="ObservDate"/>
+    <i>(MM/DD/YYYY)</i>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Quadrant location:</p>
-        <input type="text" name="quad-N" class="textbox-text"/><i>N</i>
-        <input style="margin-left: 5px;" type="text" name="quad-W" class="textbox-text"/><i>W</i>
+    <p class="textbox-text">Quadrant location:</p>
+    <input type="text" name="quad-N" class="textbox-text"/><i>N</i>
+    <input style="margin-left: 5px;" type="text" name="quad-W" class="textbox-text"/><i>W</i>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Quadrant size:</p><input type="text" name="QuadSize"/>
-        <i>m^2</i>
+    <p class="textbox-text">Quadrant size:</p><input type="text" name="QuadSize"/>
+    <i>m^2</i>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Buckthorn Stem Count:</p>
-        <input type="text" name="stem-count"/>
+    <p class="textbox-text">Buckthorn Stem Count:</p>
+    <input type="text" name="stem-count"/>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Foliar Coverage:</p>
-        <input type="text" name="folilar-cov"/>
-        <i>%</i>
+    <p class="textbox-text">Foliar Coverage:</p>
+    <input type="text" name="folilar-cov"/>
+    <i>%</i>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Stem Circumference:</p>
-        <input type="text" name="stem-circum"/>
-        <i>cm</i>
+    <p class="textbox-text">Stem Circumference:</p>
+    <input type="text" name="stem-circum"/>
+    <i>cm</i>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Habitat Description:</p>
-        <br>
-        <textarea name="Habitat" ROWS="3" cols="30"></textarea>
+    <p class="textbox-text">Habitat Description:</p>
+    <br>
+    <textarea name="Habitat" ROWS="3" cols="30"></textarea>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p class="textbox-text">Notes:</p>
-        <br>
-        <textarea name="notes" ROWS="3" cols="30"></textarea>
+    <p class="textbox-text">Notes:</p>
+    <br>
+    <textarea name="obs-notes" ROWS="3" cols="30"></textarea>
 
 </form>
 
-<form class="table">
+<form class="table2">
     <p class="textbox-text">Enter the Number of Species:</p>
     <input type="text" name="NumSpecies"/>
 
@@ -76,10 +76,49 @@
 
     <p class="textbox-text">Shanon Weiner Index:</p>
     <input type="text" name="SWI"/>
+
+    <br>
+    <br>
+
+    <p class="textbox-text">Notes</p>
+    <br>
+    <textarea name="BD-notes" ROWS="3" cols="30"></textarea>
 </form>
 
 <form class="table">
+    <p class="textbox-text">DBH of Closest Neighbor:</p>
+    <input type="text" name="DBH"/>
 
+    <br>
+    <br>
+
+    <p class="textbox-text">Distance to Closest Neighbor:</p>
+    <input type="text" name="D-Buck"/>
+
+    <br>
+    <br>
+
+    <p class="textbox-text">DBH of Closest Neighbor:</p>
+    <input type="text" name="DBH-neighbor"/>
+
+    <br>
+    <br>
+
+    <p class="textbox-text">Distance to Closest Non-Buckthorn:</p>
+    <input type="text" name="D-NonBuck"/>
+
+    <br>
+    <br>
+
+    <p class="textbox-text">DBH of Closest Non-Buckthorn:</p>
+    <input type="text" name="DBH-nonBuck"/>
+
+    <br>
+    <br>
+
+    <p class="textbox-text">Notes:</p>
+    <br>
+    <textarea name="Comp-notes" ROWS="3" cols="30"></textarea>
 </form>
 
 
