ac387a1746863536665becc319b81019da702678
jjarl12 <jonathan-jarl@bethel.edu>
2016-12-14 12:39:57 -0600

    Fixed Session Variables

--- HTMLandCSS/HomePHP.php
+++ HTMLandCSS/HomePHP.php
@@ -7,7 +7,7 @@ ini_set('display_errors', '1');
 $_SESSION['user'] = $_POST['username'];
 $_SESSION['pass'] = $_POST['password'];
 
-$_Session['con'] = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"smm") or die("Some error occurred");
+$_SESSION['con'] = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"smm") or die("Some error occurred");
 
 header("Location: {$_POST["redirect"]}");
 ?>
--- HTMLandCSS/SelectPHP.php
+++ HTMLandCSS/SelectPHP.php
@@ -5,4 +5,5 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 $ObsQuery = "select obs_ID, g_ID, date from observations";
-$_Session['result'] = mysqli_query($_Session['con'], $ObsQuery);
+$_SESSION['result'] = mysqli_query($_SESSION['con'], $ObsQuery);
+?>
