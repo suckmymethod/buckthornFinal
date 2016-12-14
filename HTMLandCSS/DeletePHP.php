5c14b2ee7601730fe63d06174f875780a5d70c8b
jjarl12 <jonathan-jarl@bethel.edu>
2016-12-14 17:44:45 -0600

    Got rid of Nathan's stupid echo... It was so dumb

--- HTMLandCSS/DeletePHP.php
+++ HTMLandCSS/DeletePHP.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 $selection = explode(' ', $_POST['deleteme']);
-echo $selection[0];
 $query = "delete from observations where obs_ID = ". $selection[0];
 $recon = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"smm") or die("Some error occurred");
 mysqli_query($recon, $query);
