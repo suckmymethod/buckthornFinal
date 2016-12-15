986936b910f28948210bc611dc31294eceded3ce
Nathan Li <nal64753@bethel.edu>
2016-12-15 16:39:16 -0600

    make query submissions

--- HTMLandCSS/newPHP.php
+++ HTMLandCSS/newPHP.php
@@ -9,7 +9,6 @@ try{
 
   //TODO
   // we need a way to get the group ID from the select menu in order to properly associate new observations with their respective groups.
-  // we need to redirect errors to the error page and display them properly from there.
   // add density field in UI
 
   //grab the observation data
@@ -49,17 +48,26 @@ try{
   $obs_query = "insert into observations " . "1" . ", " . "2" . ", " . $date . ", " . $quad_GPS . ", " . $quad_size . ", " . $stem_count . ", ";
       . ".5" . ", " . $fol_cov . ", " . $circumference . ", " . $habitat . ", " . $obs_notes . ";";
   $bio_query = "insert into biodiversity " . "1" . ", " . $date . ", " . $weiner . ", " . $BD_notes . ";";
-//  $spec_query =  "insert into species " . $
+//  $spec_query =  "insert into species " .
   $comp_query = "insert into competition " . "1" . ", " . "2" . ", " $date . ", " . $DBH . ", " . $D_close . ", " . $DBH_close . ", "
       . $D_non_buck . ", " . $DBH_non_buck . ", " . $comp_notes . ";";
 
+  //we have the queries, now to execute
+  $recon = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"smm") or die("Some error occurred");
+  mysqli_query($recon, $obs_query);
+  mysqli_query($recon, $bio_query);
+  //TODO mysqli_query($recon, $spec_query);
+  mysqli_query($recon, $comp_query);
 
+
+
+  // redirect to submission/confirm pages
+  header("Location: {$_POST["submit-new"]}");
 } catch (Exception $e) {
-  header("Location: {$_POST["error"]}");
+  //header("Location: {$_POST["error"]}");
 }
 //^^^^all this pulls vars from the webpage to be placed into the database
 
 
-// redirect to submission/confirm pages
-header("Location: {$_POST["submit-new"]}");
+
 ?>
