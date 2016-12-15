196c1fe6e8b4882aacdaf257f31c5d7cdc270fc9
k-larsen <CK@bethel-wifi.dhcp-128-242.vlan.net.its.bethel.edu>
2016-12-14 20:33:40 -0600

    Delete page

---
+++ HTMLandCSS/Del_Confirm.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Submission</title>
+    <link rel="stylesheet" href="Stylesheet.css" type="text/css">
+</head>
+
+<body>
+<header>
+    <img class="logo" alt="Bethel University" src="https://www.bethel.edu/cdn/images/logo.png">
+    <h1>Delete Submission</h1>
+
+</header>
+
+<p class="left-btn">The observation has been deleted.</p>
+<br>
+<a href="Home.php">
+    <div class="btn left-btn">Home</div>
+</a>
+
+<footer>
+    <div class="foot-text">
+        <a href="Home.php"><p>Home</p></a>
+        <p class="foot-CR">© SMM 2016</p>
+    </div>
+</footer>
+</body>
+</html>
