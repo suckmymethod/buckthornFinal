7e0bb8b096d4a6a9f89eae3b590ef04bc857ad3e
Nathan Li <nal64753@bethel.edu>
2016-12-16 08:52:20 -0600

    revert back to lowercase

--- HTMLandCSS/Select_Data.php
+++ HTMLandCSS/Select_Data.php
@@ -11,7 +11,7 @@ $SelectedObs = explode(' ', $_POST['PickedObs']);
 $obsQuery = "Select * from observations where obs_ID = ". $SelectedObs[0];
 $biodiQuery = "Select * from biodiversity where obs_ID = ". $SelectedObs[0];
 $compQuery = "Select * from competition where obs_ID = ". $SelectedObs[0];
-$recon = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"SMM") or die("Some error occurred");
+$recon = mysqli_connect("localhost",$_SESSION['user'],$_SESSION['pass'],"smm") or die("Some error occurred");
 $_SESSION['obs'] = mysqli_query($recon, $obsQuery);
 $_SESSION['bio'] = mysqli_query($recon, $biodiQuery);
 $_SESSION['comp'] = mysqli_query($recon, $compQuery);
